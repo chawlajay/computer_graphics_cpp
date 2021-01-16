ccafae753da12b1da51be1f5b8738d20fbb99074
chawlajay <chawlajay111@gmail.com>
2021-01-16 12:28:31 +0530

    car is moving, with delay() and cleardevice() function

--- interesting_applications/moving_car.cpp
+++ interesting_applications/moving_car.cpp
@@ -11,32 +11,35 @@ void draw_moving_car(){
         setcolor(YELLOW); // color of car
 
         // lines for bonnet and body of car
-        line(0 + i, 300, 210 + i, 300);         // lower horizontal line
-        line(50 + i, 300, 75 + i, 270);         // left inclined line
-        line(75 + i, 270, 150 + i, 270);        // upper horizontal line
-        line(150 + i, 270, 165 + i, 300);       // right inclined line
-        line(0 + i, 300, 0 + i, 330);           // left vertical line
-        line(210 + i, 300, 210 + i, 330);       // right vertical line
+        line(0 + i, 200, 210 + i, 200);         // lower horizontal line
+        line(50 + i, 200, 75 + i, 170);         // left inclined line
+        line(75 + i, 170, 150 + i, 170);        // upper horizontal line
+        line(150 + i, 170, 165 + i, 200);       // right inclined line
+        line(0 + i, 200, 0 + i, 230);           // left vertical line
+        line(210 + i, 200, 210 + i, 230);       // right vertical line
 
         // For left wheel of car
-        circle(65 + i, 330, 15);
-        circle(65 + i, 330, 2);
+        circle(65 + i, 230, 15);
+        circle(65 + i, 230, 2);
 
         // For right wheel of car
-        circle(145 + i, 330, 15);
-        circle(145 + i, 330, 2);
+        circle(145 + i, 230, 15);
+        circle(145 + i, 230, 2);
 
         // Line left of left wheel
-        line(0 + i, 330, 50 + i, 330);
+        line(0 + i, 230, 50 + i, 230);
 
         // Line middle of both wheel
-        line(80 + i, 330, 130 + i, 330);
+        line(80 + i, 230, 130 + i, 230);
 
         // Line right of right wheel
-        line(210 + i, 330, 160 + i, 330);
+        line(210 + i, 230, 160 + i, 230);
 
         delay(100);
 
+        // To erase previous drawn car
+        // use cleardevice() function
+        cleardevice();
     }
 
     getch();
