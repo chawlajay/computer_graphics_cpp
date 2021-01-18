484e362ec0954c093dd01686c0fe0b7079582386
chawlajay <chawlajay111@gmail.com>
2021-01-18 17:13:14 +0530

    add front wall colored texture done

--- interesting_applications/house.cpp
+++ interesting_applications/house.cpp
@@ -7,8 +7,13 @@ void house(){
     int gd=DETECT,gm;
     initgraph(&gd, &gm, "");
 
+    setfillstyle(2,CYAN);
     rectangle(100,200,400,400);     // front wall big rectangle
+    floodfill(150,250,WHITE);
+
+    setfillstyle(3,GREEN);
     rectangle(100,220,400,220);     // front wall small rectangle
+    floodfill(150,210,WHITE);
 
     line(400,200,550,150);          //side wall upper line
     line(400,220,550,170);          //side wall middle line
@@ -19,8 +24,8 @@ void house(){
     line(300,50,400,200);           //dome middle slant line
     line(300,50,550,150);           //dome right slant line
 
-    rectangle(220,300,280,400); //door rectangle
-    arc(250,300,0,180,30);         // arc on door
+    rectangle(220,300,280,400);     // door rectangle
+    arc(250,300,0,180,30);          // arc on door
     getch();
     closegraph();
 
