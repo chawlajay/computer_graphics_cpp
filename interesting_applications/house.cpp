238011ba362fbcf6bd2e2bfc02bef04749bc04da
chawlajay <chawlajay111@gmail.com>
2021-01-18 17:00:25 +0530

    add side wall of house

--- interesting_applications/house.cpp
+++ interesting_applications/house.cpp
@@ -9,8 +9,12 @@ void house(){
 
     rectangle(100,200,400,400);     // front wall big rectangle
     rectangle(100,220,400,220);     // front wall small rectangle
-
-
+    
+    line(400,200,550,150);          //side wall upper line
+    line(400,220,550,170);          //side wall middle line
+    line(400,400,550,350);          //side wall lower line
+    line(550,150,550,350);          //side wall vertical line
+	
     getch();
     closegraph();
 
