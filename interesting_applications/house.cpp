880b6e3fd0545e15c8d4118a73e62d3b0ad1095b
chawlajay <chawlajay111@gmail.com>
2021-01-18 17:07:25 +0530

    add 1 door on house

--- interesting_applications/house.cpp
+++ interesting_applications/house.cpp
@@ -9,16 +9,18 @@ void house(){
 
     rectangle(100,200,400,400);     // front wall big rectangle
     rectangle(100,220,400,220);     // front wall small rectangle
-    
+
     line(400,200,550,150);          //side wall upper line
     line(400,220,550,170);          //side wall middle line
     line(400,400,550,350);          //side wall lower line
     line(550,150,550,350);          //side wall vertical line
-	
+
     line(300,50,100,200);           //dome left slant line
     line(300,50,400,200);           //dome middle slant line
     line(300,50,550,150);           //dome right slant line
 
+    rectangle(220,300,280,400); //door rectangle
+    arc(250,300,0,180,30);         // arc on door
     getch();
     closegraph();
 
